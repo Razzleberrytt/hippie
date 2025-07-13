22b4014d5515b6eea40e3b9e46ae5d1fc802580a
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 17:18:22 -0400

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Razzleberrytt.github.io/hippie',
+  site: 'https://razzleberrytt.github.io/hippie',
   base: '/hippie/',
 });
