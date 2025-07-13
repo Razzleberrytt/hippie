75a272e1e87ce88e7b3ab57cec5e9eecedb579e4
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 19:07:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
