50091865f3db5d8327b92a1de8b8180eaaa910de
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 17:27:21 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import postcss from './postcss.config.cjs'; // 👈 optional if needed manually
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://razzleberrytt.github.io/hippie',
+  site: 'https://Razzleberrytt.github.io/hippie',
   base: '/hippie/',
 });
