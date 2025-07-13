2861c655e608ec794c5cadf75d69b90612f695a9
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 19:44:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github-pages';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://razzleberrytt.github.io/hippie/', // Ensure this is your GitHub Pages site path
-  base: '/hippie/', // Required for subpath deployment
+  site: 'https://razzleberrytt.github.io/hippie/',
+  base: '/hippie/',
   output: 'static',
-  adapter: github(),
   integrations: [tailwind()],
 });
