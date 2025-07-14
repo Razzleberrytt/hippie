af63c1385bfc8dd125405d69a3787e06cf8297a6
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 22:46:24 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
-  adapter: staticAdapter(),
-  output: 'static',
+  output: 'server'
 });
