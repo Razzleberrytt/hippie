8fb049d3a366678ebec7f022bcd1d107d6359f68
Willie Randolph <RazzleberryT@gmail.com>
2025-07-13 20:05:03 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   site: 'https://razzleberrytt.github.io/hippie/',
   base: '/hippie/',
   output: 'static',
+  adapter: staticAdapter(),
   integrations: [tailwind()],
 });
